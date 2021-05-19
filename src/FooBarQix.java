559d5df1fcc4931ffe3d6fe67e726bb056959753
DyhiaNaima <chchnaima@gmail.com>
2021-05-19 14:50:12 +0200

    For each digit 3, 5, 7, add Foo, Bar, Qix in the digits order and each 0 must be replaced by char *

--- src/FooBarQix.java
+++ src/FooBarQix.java
@@ -5,16 +5,34 @@ public class FooBarQix {
 	public static String fooBarQix(int number) {
 		String result = "";
 		if(number % 3 == 0) {
-			result = "Foo";
-		}else if(number % 5 == 0) {
-			result="Bar";			
-		}else if(number % 7 == 0) {
-			result="Qix";			
+			result += "Foo";
+		}if(number % 5 == 0) {
+			result +="Bar";			
+		}if(number % 7 == 0) {
+			result +="Qix";			
 		}
 		if(!result.isEmpty()) {
 			return result;
 		}else return String.valueOf(number);
 		
+	}
+	public static  String containsFooBarQix(int number) {
+		String result = "";
+		String zero="";
+		String chaine = String.valueOf(number);
+		for(int i=0; i<chaine.length(); i++) {
+			switch(chaine.charAt(i)) {
+				case '0': zero +="*";
+				break;
+				case '3': result +="Foo";
+				break;
+				case '5': result +="Bar";
+				break;
+				case '7': result +="Qix"; 
+				break;
+			}
+		}
+		return zero+result;
 	}
 	
 
@@ -24,7 +42,7 @@ public class FooBarQix {
 			Scanner sc = new Scanner(System.in);
 			System.out.println("Veuillez saisir un nombre :");
 			int str = sc.nextInt();
-			System.out.println(fooBarQix(str));
+			System.out.println(fooBarQix(str)+containsFooBarQix(str));
 		}
 
 	}
